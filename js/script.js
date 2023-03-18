44fe053324ddafe45758fdb801e3d8581f8c20d8
Muhammad Febrian <technogamers543@gmail.com>
2023-03-18 14:09:15 +0800

    make a transition while the browser is loading

---
+++ js/script.js
@@ -0,0 +1,16 @@
+const title = document.querySelector("header h1");
+const main = document.querySelector("main");
+const cards = document.querySelectorAll(".card");
+
+window.onload = () => {
+  title.classList.add("translateY-0");
+  title.classList.add("opacity-1");
+
+  setTimeout(() => {
+    for (let i = 0; i < 5; i++) {
+      cards[i].style.transition = `all 0.5s ${(i + 1) * 0.2}s ease`;
+      cards[i].style.transform = "translateY(0)";
+      cards[i].style.opacity = 1;
+    }
+  }, 450);
+};
