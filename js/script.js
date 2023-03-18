99440dd99713de27bd8bbc1cf803337b4ab02984
Muhammad Febrian <technogamers543@gmail.com>
2023-03-18 20:24:34 +0800

    make cards animation by using aos library

--- js/script.js
+++ js/script.js
@@ -7,10 +7,18 @@ window.onload = () => {
   title.classList.add("opacity-1");
 
   setTimeout(() => {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 4; i++) {
       cards[i].style.transition = `all 0.5s ${(i + 1) * 0.2}s ease`;
       cards[i].style.transform = "translateY(0)";
       cards[i].style.opacity = 1;
     }
   }, 450);
 };
+
+for (let i = 4; i < cards.length; i++) {
+  cards[i].setAttribute("data-aos", "fade-up");
+
+  if (i % 2 !== 0) {
+    cards[i].setAttribute("data-aos-delay", 200);
+  }
+}
