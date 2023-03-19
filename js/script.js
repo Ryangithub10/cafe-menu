f75c197dd83c9c02686cb235cf760d421317fb31
Muhammad Febrian <technogamers543@gmail.com>
2023-03-19 21:20:19 +0800

    refactor code

--- js/script.js
+++ js/script.js
@@ -2,23 +2,27 @@ const title = document.querySelector(".title");
 const main = document.querySelector("main");
 const cards = document.querySelectorAll(".card");
 
-window.onload = () => {
-  title.classList.add("translateY-0");
-  title.classList.add("opacity-1");
+const timeSleep = (ms) => new Promise((res) => setTimeout(res, ms));
+const addClass = (element, cls) => element.classList.add(cls);
+const setAttribute = (element, atr, atrValue) =>
+  element.setAttribute(atr, atrValue);
 
-  setTimeout(() => {
-    for (let i = 0; i < 4; i++) {
-      cards[i].style.transition = `all 0.5s ${(i + 1) * 0.2}s ease`;
-      cards[i].style.transform = "translateY(0)";
-      cards[i].style.opacity = 1;
-    }
-  }, 450);
+window.onload = async () => {
+  addClass(title, "translateY-0");
+  addClass(title, "opacity-1");
+
+  await timeSleep(450);
+  for (let i = 0; i < 4; i++) {
+    cards[i].style.transition = `all 0.5s ${(i + 1) * 0.12}s ease`;
+    cards[i].style.transform = "translateY(0)";
+    cards[i].style.opacity = 1;
+  }
 };
 
 for (let i = 4; i < cards.length; i++) {
-  cards[i].setAttribute("data-aos", "fade-up");
+  setAttribute(cards[i], "data-aos", "fade-up");
 
   if (i % 2 !== 0) {
-    cards[i].setAttribute("data-aos-delay", 200);
+    setAttribute(cards[i], "data-aos-delay", 200);
   }
 }
