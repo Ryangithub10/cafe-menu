d34141dc39f0b9ac4bff53997b32e7ae16247380
Muhammad Febrian <technogamers543@gmail.com>
2023-03-26 05:16:35 +0800

    get coffee data from api

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
+// get element
 const title = document.querySelector(".title");
-const cards = document.querySelectorAll(".card");
+const main = document.querySelector("main");
 
+// function
+// oneline function
 const timeSleep = (ms) => new Promise((res) => setTimeout(res, ms));
 const addClass = (element, ...clss) =>
   clss.forEach((cls) => element.classList.add(cls));
@@ -9,20 +12,56 @@ const setAttribute = (element, atr, atrValue) =>
 const style = (element, styles) =>
   styles.forEach((prop) => (element.style[prop[0]] = prop[1]));
 
-window.onload = async () => {
-  addClass(title, "translateY-0", "opacity-1");
+// multiline function
+const getData = async (url) => {
+  const res = await fetch(url);
+  const data = await res.json();
 
-  await timeSleep(450);
-  for (let i = 0; i < 4; i++) {
-    style(cards[i], [
-      ["transition", `all 0.5s ${(i + 1) * 0.12}s ease`],
-      ["transform", "translateY(0)"],
-      ["opacity", 1],
-    ]);
-  }
+  return data;
 };
 
-for (let i = 4; i < cards.length; i++) {
-  setAttribute(cards[i], "data-aos", "fade-up");
-  if (i % 2 !== 0) setAttribute(cards[i], "data-aos-delay", 200);
-}
+const createCard = ({ title, description, image }) => {
+  return `
+    <div class="card">
+      <figure class="thumb-container">
+        <img
+          src="${image}"
+          alt="${title}"
+          class="thumb"
+        />
+        <figcaption class="coffee-name">${title}</figcaption>
+      </figure>
+
+      <article class="card-body">
+        <p class="price">Rp. 10.000</p>
+        <p class="card-desc">
+          ${description}
+        </p>
+      </article>
+    </div>
+  `;
+};
+
+// manipulation html
+window.onload = async () => {
+  const hotCoffees = await getData("https://api.sampleapis.com/coffee/hot");
+  const coldCoffees = await getData("https://api.sampleapis.com/coffee/iced");
+  hotCoffees.forEach((coffee) => (main.innerHTML += createCard(coffee)));
+  coldCoffees.forEach((coffee) => (main.innerHTML += createCard(coffee)));
+  const cards = document.querySelectorAll(".card")
+
+  addClass(title, "translateY-0", "opacity-1");
+  for (const [i, card] of cards.entries()) {
+    if (i < 4) {
+      style(card, [
+        ["transition", `all 0.5s ${(i + 1) * 0.12}s ease`],
+        ["transform", "translateY(0)"],
+        ["opacity", 1],
+      ]);
+      continue;
+    }
+
+    setAttribute(card, "data-aos", "fade-up");
+    if (i % 2 !== 0) setAttribute(card, "data-aos-delay", 200);
+  }
+};
